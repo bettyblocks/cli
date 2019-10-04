caa2daccefc4416ebcbe6f303f98db4bcb2ca566
Lars Hisken <lars.hisken@bettyblocks.com>
2019-10-04 13:04:26 +0200

    refactor: remove partials

--- src/bb-components-build.ts
+++ src/bb-components-build.ts
@@ -5,7 +5,6 @@ import { validateDuplicateNames } from './utils/validation';
 
 import { validateSchema as validateComponentSchema } from './validations/component';
 import { validateSchema as validatePrefabSchema } from './validations/prefab';
-import { validateSchema as validatePartialSchema } from './validations/partial';
 
 import transpile from './utils/transpile';
 import readScripts from './utils/readScripts';
@@ -80,41 +79,10 @@ const buildPrefabs: (rootDir: string) => Promise<void> = async (
   await outputJson(`${distDir}/prefabs.json`, output);
 };
 
-const buildPartials: (rootDir: string) => Promise<void> = async (
-  rootDir: string,
-): Promise<void> => {
-  const srcDir = `${rootDir}/src/partials`;
-  const distDir = `${rootDir}/dist`;
-  const exists = await pathExists(srcDir);
-
-  if (!exists) {
-    return await outputJson(`${distDir}/partials.json`, []);
-  }
-
-  const partialFiles: string[] = await readScripts(srcDir);
-
-  const promises = partialFiles.map(
-    async (file: string): Promise<Prefab> => {
-      const code = await readFile(`${srcDir}/${file}`, 'utf-8');
-
-      return Function(`return ${code}`)();
-    },
-  );
-
-  const output: Prefab[] = await Promise.all(promises);
-
-  validateDuplicateNames(output);
-  validatePartialSchema(output);
-
-  await mkdir(distDir, { recursive: true });
-  await outputJson(`${distDir}/partials.json`, output);
-};
-
 (async (): Promise<void> => {
   try {
     await buildComponents(rootDir);
     await buildPrefabs(rootDir);
-    await buildPartials(rootDir);
     console.info('Success');
   } catch ({ message }) {
     console.error(message);
