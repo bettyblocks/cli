6dd36b72a7772236649e22e40b5f126d7dd19e79
Bas Dirks <bas.dirks@bettyblocks.com>
2019-10-08 14:06:57 +0200

    feat: initial configuration of eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,40 +2,13 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:react/recommended',
     'prettier/@typescript-eslint',
-    'airbnb',
-    'airbnb/hooks',
+    'airbnb/base',
     'plugin:prettier/recommended',
   ],
-  plugins: ['react', '@typescript-eslint', 'prettier'],
-  env: {
-    browser: true,
-    jasmine: true,
-    jest: true,
-  },
+  plugins: ['@typescript-eslint', 'prettier'],
   rules: {
     'prettier/prettier': ['error', { singleQuote: true }],
-    'react/jsx-filename-extension': [1, { extensions: ['tsx'] }],
-    'react/jsx-wrap-multilines': 0,
-    'react/jsx-one-expression-per-line': 0,
-    'react/jsx-props-no-spreading': 0,
-    'react/jsx-curly-newline': 0,
-    'sort-keys': 1,
-  },
-  settings: {
-    react: {
-      pragma: 'React',
-      version: 'detect',
-    },
-    'import/resolver': {
-      node: {
-        extensions: ['.js', '.jsx', '.ts', '.tsx'],
-      },
-    },
   },
   parser: '@typescript-eslint/parser',
-  globals: {
-    process: true,
-  },
 };
