19ca49395c79410eda749e399be27b6b94bfe783
Bas Dirks <bas.dirks@bettyblocks.com>
2019-10-09 10:39:53 +0200

    refactor: split pure and impure code

--- src/utils/checkUpdateAvailable.ts
+++ src/utils/checkUpdateAvailable.ts
@@ -1,27 +1,35 @@
 import chalk from 'chalk';
 import semver from 'semver';
-import fetch from 'node-fetch';
+import fetch, { Response } from 'node-fetch';
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const { version, name } = require('../../package.json');
+const { version: localVersion, name } = require('../../package.json');
+
+const logAvailableUpdate: (upstreamVersion: string) => void = (
+  upstreamVersion: string,
+): void => {
+  console.log(
+    `${name} update available from ${chalk.greenBright(
+      localVersion,
+    )} to ${chalk.greenBright(upstreamVersion)}.`,
+  );
+};
 
 export default async (): Promise<void> => {
+  let res: Response;
+
   try {
-    const res = await fetch(
+    res = await fetch(
       'https://api.github.com/repos/bettyblocks/cli/releases/latest',
     );
-
-    const { tag_name: tagName } = await res.json();
-    const upstreamVersion: string = tagName.substring(1);
-
-    if (semver.lt(version, upstreamVersion)) {
-      console.log(
-        `${name} update available from ${chalk.greenBright(
-          version,
-        )} to ${chalk.greenBright(upstreamVersion)}.`,
-      );
-    }
   } catch {
-    // Fail silently!
+    return;
+  }
+
+  const { tag_name: tagName } = await res.json();
+  const upstreamVersion: string = tagName.substring(1);
+
+  if (semver.lt(localVersion, upstreamVersion)) {
+    logAvailableUpdate(upstreamVersion);
   }
 };
