b8543f0827b7cd56f76989f841ecc8b3b683ec97
Niek Nijland <niek.nijland@bettyblocks.com>
2019-10-14 16:58:03 +0200

    fix: version command

--- src/bb.ts
+++ src/bb.ts
@@ -1,12 +1,14 @@
 #!/usr/bin/env node
 
 import program from 'commander';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { version } = require('../package.json');
 
 const availableCommands = ['components', 'help'];
 
 program
   .description('The Betty Blocks CLI')
-  .version('1.0.0')
+  .version(version)
   .command('components <cmd>', 'manage your component sets')
   .on('command:*', ([command]: string[]): void => {
     if (!availableCommands.includes(command)) {
