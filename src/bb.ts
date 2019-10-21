ddb16729b6171fe32ee0eab2fd192155da6617f8
Bas Dirks <bas.dirks@bettyblocks.com>
2019-10-21 10:14:48 +0200

    fix: type commands to prevent mistakes

--- src/bb.ts
+++ src/bb.ts
@@ -1,19 +1,20 @@
 #!/usr/bin/env node
 
 import program from 'commander';
+import { CommandBB } from './types';
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const { version } = require('../package.json');
+const { version }: { version: string } = require('../package.json');
 
-const availableCommands = ['components', 'help'];
+const availableCommands: CommandBB[] = ['components', 'help'];
 
 program
   .description('The Betty Blocks CLI')
   .version(version)
   .command('components <cmd>', 'manage your component sets')
   .on('command:*', ([command]: string[]): void => {
-    if (!availableCommands.includes(command)) {
-      throw new Error(`Invalid command: ${program.args.join(' ')}\n`);
+    if (!availableCommands.includes(command as CommandBB)) {
+      throw new Error(`Invalid command: ${command}`);
     }
   })
   .parse(process.argv);
