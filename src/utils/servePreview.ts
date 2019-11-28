275475d8a68d3f9705caee840d07038809520c34
Robin Mulder <robin.mulder@bettyblocks.com>
2019-11-28 14:32:47 +0100

    fix: directory check

--- src/utils/servePreview.ts
+++ src/utils/servePreview.ts
@@ -1,18 +1,40 @@
 import { createServer, Server, IncomingMessage, ServerResponse } from 'http';
 import handler from 'serve-handler';
 import path from 'path';
+import fs from 'fs';
 
-export default (port: number): void => {
+export default async (port: number): Promise<void> => {
   const nodeModulesDir = (process.mainModule as { paths: string[] }).paths[1];
-
-  const server: Server = createServer(
-    (response: IncomingMessage, request: ServerResponse): Promise<void> =>
-      handler(response, request, {
-        public: path.join(nodeModulesDir, '../../preview/build'),
-      }),
+  const yarnInstallDir: string = path.join(
+    nodeModulesDir,
+    '../../preview/build',
   );
+  const npmInstallDir: string = path.join(
+    nodeModulesDir,
+    './@betty-blocks/preview/build',
+  );
+  try {
+    const dirCheckNpm: void = await fs.promises.access(
+      npmInstallDir,
+      fs.constants.F_OK,
+    );
 
-  server.listen(port, (): void => {
-    console.info(`Serving the preview at http://localhost:${port}`);
-  });
+    const location: string =
+      dirCheckNpm !== undefined ? yarnInstallDir : npmInstallDir;
+
+    const server: Server = createServer(
+      (response: IncomingMessage, request: ServerResponse): Promise<void> =>
+        handler(response, request, {
+          public: location,
+        }),
+    );
+
+    server.listen(port, (): void => {
+      console.info(`Serving the preview at http://localhost:${port}`);
+    });
+  } catch {
+    console.error(
+      'Cannot find the preview directory, please try again after upgrading the CLI to the latest version.',
+    );
+  }
 };
