d8490d88425b1fe2a7434dbbd82d72c418400cd3
Bas Dirks <bas.dirks@protonmail.com>
2020-01-02 14:20:20 +0100

    feat: add prompt on init, create YAML structure

--- src/bb-components-init.ts
+++ src/bb-components-init.ts
@@ -3,10 +3,13 @@
 import chalk from 'chalk';
 import program, { CommanderStatic } from 'commander';
 import { copy, existsSync, move } from 'fs-extra';
-import inquirer from 'inquirer';
+import { prompt } from 'inquirer';
 import path from 'path';
+import { parse, stringify } from 'yaml';
 
 import { checkUpdateAvailableCLI } from './utils/checkUpdateAvailable';
+
+const VALID_NAME_PATTERN = /[a-z0-9-_]+\/[a-z0-9-_]/;
 
 const files = [
   'package.json',
@@ -34,10 +37,51 @@ if (args.length > 0) {
 
   const cwd = process.cwd();
 
-  // Explain name
-  // Prompt for name
-  // Prompt for scope
+  const {
+    name,
+    isPublic,
+    isPublicConfirmed,
+  }: {
+    name: string;
+    isPublic: boolean;
+    isPublicConfirmed: boolean;
+  } = await prompt([
+    {
+      name: 'name',
+      message: 'Name',
+      prefix:
+        'The name of your component set should start with @, followed by your organization id, /, and the name of your set.',
+      validate: (name: string): true | string =>
+        VALID_NAME_PATTERN.test(name) ||
+        'Make sure the name starts with @, followed by your organization id, /, and the name of your set.',
+    },
+    {
+      default: false,
+      filter: (raw: string): boolean => raw.toLowerCase() === 'public',
+      message: 'Is your component set public?',
+      name: 'isPublic',
+      type: 'confirm',
+    },
+    {
+      default: false,
+      name: 'isPublicConfirmed',
+      message:
+        'Are you ABSOLUTELY SURE that your component set should be public? Once published as such, it cannot be unpublished.',
+      type: 'confirm',
+      when: ({ isPublic }): boolean => isPublic,
+    },
+  ]);
+
   // Copy assets into place
+
+  const yaml = stringify({
+    dependencies: {},
+    public: isPublic && isPublicConfirmed,
+    name,
+    version: '0.1.0',
+  });
+
+  console.log(yaml);
   // Write bettyblocks.yaml
 
   console.log(cwd);
