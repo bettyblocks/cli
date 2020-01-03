711eb9cce2bbf564f4c965025b9e28600272fbed
Bas Dirks <bas.dirks@protonmail.com>
2020-01-03 16:22:45 +0100

    fix: prevent init from overwriting existing files

--- src/bb-components-init.ts
+++ src/bb-components-init.ts
@@ -5,24 +5,21 @@ import program, { CommanderStatic } from 'commander';
 import { promises } from 'fs';
 import { copy, move } from 'fs-extra';
 import { prompt } from 'inquirer';
-import path from 'path';
+import { join } from 'path';
 import { stringify } from 'yaml';
 
 /* internal dependencies */
 import { checkUpdateAvailableCLI } from './utils/checkUpdateAvailable';
 
-const { writeFile } = promises;
+const { readdir, stat, writeFile } = promises;
+
+const CWD = process.cwd();
 
 const VALID_NAME_PATTERN = /[a-z0-9-_]+\/[a-z0-9-_]/;
 
-const DOT_FILES = [
-  'package.json',
-  '.eslintignore',
-  '.eslintrc.json',
-  '.gitignore',
-  '.prettierignore',
-  '.prettierrc.json',
-];
+const ASSET_PATH = join(__dirname, '../assets/component-set');
+const YAML_PATH = join(CWD, 'bettyblocks.yaml');
+const SRC_PATH = join(CWD, 'src');
 
 /* process arguments */
 
@@ -39,71 +36,82 @@ if (args.length > 0) {
 (async (): Promise<void> => {
   await checkUpdateAvailableCLI();
 
-  const cwd = process.cwd();
-
-  const {
-    name,
-    isPublic,
-    isPublicConfirmed,
-  }: {
-    name: string;
-    isPublic: boolean;
-    isPublicConfirmed: boolean;
-  } = await prompt([
-    {
-      name: 'name',
-      message: 'Name',
-      prefix:
-        'The name of your component set should start with @, followed by your organization id, /, and the name of your set.',
-      validate: (nameValue: string): true | string =>
-        VALID_NAME_PATTERN.test(nameValue) ||
-        'Make sure the name starts with @, followed by your organization id, /, and the name of your set. For example: @betty-blocks/layout',
-    },
-    {
-      default: false,
-      message: 'Is this component set public?',
-      name: 'isPublic',
-      type: 'confirm',
-    },
-    {
-      default: false,
-      name: 'isPublicConfirmed',
-      message:
-        'Are you ABSOLUTELY SURE that your component set should be public? Once published as such, it cannot be unpublished.',
-      type: 'confirm',
-      when: ({ isPublic: isPublicValue }): boolean => isPublicValue,
-    },
-  ]);
-
-  const yaml = stringify({
-    dependencies: {},
-    name,
-    public: isPublic && isPublicConfirmed,
-    version: '0.1.0',
-  });
-
   try {
-    await Promise.all([
-      copy(path.join(__dirname, '../assets/component-set'), cwd),
-      writeFile(`${cwd}/bettyblocks.yaml`, yaml, { encoding: 'utf-8' }),
-    ]);
-
-    await Promise.all(
-      DOT_FILES.map(fileName =>
-        move(`${cwd}/__${fileName}`, `${cwd}/${fileName}`),
-      ),
-    );
+    await stat(YAML_PATH);
 
     console.log(
       chalk.green(
-        `\nComponent set succesfully created in directory '${cwd}'.\n`,
+        `\nThe current working directory already contains a component set.\n`,
       ),
     );
-  } catch ({ message }) {
-    throw Error(
-      chalk.red(
-        `\nCould not create component set in directory ${cwd}: ${message}.\n`,
-      ),
-    );
+  } catch {
+    const {
+      name,
+      isPublic,
+      isPublicConfirmed,
+    }: {
+      name: string;
+      isPublic: boolean;
+      isPublicConfirmed: boolean;
+    } = await prompt([
+      {
+        name: 'name',
+        message: 'Name',
+        prefix:
+          'The name of your component set should start with @, followed by your organization id, /, and the name of your set.',
+        validate: (nameValue: string): true | string =>
+          VALID_NAME_PATTERN.test(nameValue) ||
+          'Make sure the name starts with @, followed by your organization id, /, and the name of your set. For example: @betty-blocks/layout',
+      },
+      {
+        default: false,
+        message: 'Is this component set public?',
+        name: 'isPublic',
+        type: 'confirm',
+      },
+      {
+        default: false,
+        name: 'isPublicConfirmed',
+        message:
+          'Are you ABSOLUTELY SURE that your component set should be public? Once published as such, it cannot be unpublished.',
+        type: 'confirm',
+        when: ({ isPublic: isPublicValue }): boolean => isPublicValue,
+      },
+    ]);
+
+    const yaml = stringify({
+      dependencies: {},
+      name,
+      public: isPublic && isPublicConfirmed,
+      version: '0.1.0',
+    });
+
+    try {
+      await writeFile(YAML_PATH, yaml, { encoding: 'utf-8' });
+      await copy(ASSET_PATH, CWD, { overwrite: false });
+
+      const filesCWD = await readdir(CWD);
+
+      await Promise.all(
+        filesCWD
+          .filter((file: string): boolean => file.startsWith('__'))
+          .map(
+            (file: string): Promise<void> =>
+              move(join(CWD, file), join(CWD, file.replace('__', ''))),
+          ),
+      );
+
+      console.log(
+        chalk.green(
+          `\nComponent set succesfully created in the current working directory.\n`,
+        ),
+      );
+    } catch ({ message }) {
+      throw Error(
+        chalk.red(
+          `\nCould not create component set in the current working directory: ${message}.\n`,
+        ),
+      );
+    }
   }
 })();
