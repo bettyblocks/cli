49d082d2fe0fc030ba3737be373ca5c4f686dcb8
Bas Dirks <bas.dirks@protonmail.com>
2020-01-07 14:12:25 +0100

    fix: clean up import

--- src/bb-components-init.ts
+++ src/bb-components-init.ts
@@ -4,7 +4,7 @@ import chalk from 'chalk';
 import program, { CommanderStatic } from 'commander';
 import { F_OK } from 'constants';
 import { promises } from 'fs';
-import { copy, ensureFile, mkdirp } from 'fs-extra';
+import { copy, ensureFile } from 'fs-extra';
 import { Answers } from 'inquirer';
 import { join } from 'path';
 import { stringify } from 'yaml';
