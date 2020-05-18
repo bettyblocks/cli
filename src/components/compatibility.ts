6a18e4b78852868eb0af4f607efebfed08e73120
Léon de Jong <leon.de.jong@bettyblocks.com>
2020-05-18 17:35:23 +0200

    feat: implement initial version of component compatibility

---
+++ src/components/compatibility.ts
@@ -0,0 +1,106 @@
+/* eslint-disable no-param-reassign */
+import {
+  createArrayLiteral,
+  createNodeArray,
+  createObjectLiteral,
+  createPropertyAssignment,
+  createStatement,
+  createStringLiteral,
+  isPropertyAccessExpression,
+  isSourceFile,
+  Node,
+  SourceFile,
+  TransformationContext,
+  Transformer,
+  TransformerFactory,
+  transpileModule,
+  visitEachChild,
+  visitNode,
+  Visitor,
+} from 'typescript';
+
+export interface Component {
+  functions: string[];
+  events: string[];
+}
+
+const isComponent = (value: unknown): value is Component => {
+  if (typeof value === 'object' && value !== null) {
+    const { functions, events } = value as Component;
+
+    return Array.isArray(functions) && Array.isArray(events);
+  }
+
+  return false;
+};
+
+const compatibilityTransformer = (): TransformerFactory<SourceFile> => (
+  context: TransformationContext,
+): Transformer<SourceFile> => {
+  const functions: string[] = [];
+  const triggers: string[] = [];
+
+  const visit: Visitor = (node: Node): Node => {
+    if (
+      isPropertyAccessExpression(node) &&
+      node.getText() === 'B.defineFunction'
+    ) {
+      const name = node.parent
+        .getChildAt(2)
+        .getChildAt(0)
+        .getText();
+      functions.push(name.replace(/'/g, ''));
+    }
+
+    if (
+      isPropertyAccessExpression(node) &&
+      node.getText() === 'B.triggerEvent'
+    ) {
+      const name = node.parent
+        .getChildAt(2)
+        .getChildAt(0)
+        .getText();
+      triggers.push(name.replace(/'/g, ''));
+    }
+
+    return visitEachChild(node, visit, context);
+  };
+
+  return (node: SourceFile): SourceFile => {
+    visitNode(node, visit);
+
+    if (isSourceFile(node)) {
+      node.statements = createNodeArray([
+        createStatement(
+          createObjectLiteral([
+            createPropertyAssignment(
+              createStringLiteral('functions'),
+              createArrayLiteral(functions.map(n => createStringLiteral(n))),
+            ),
+            createPropertyAssignment(
+              createStringLiteral('events'),
+              createArrayLiteral(triggers.map(n => createStringLiteral(n))),
+            ),
+          ]),
+        ),
+      ]);
+    }
+
+    return node;
+  };
+};
+
+export default (code: string): Component => {
+  const { outputText } = transpileModule(code, {
+    transformers: { before: [compatibilityTransformer()] },
+  });
+
+  const { length } = outputText;
+  const component = JSON.parse(outputText.slice(1, length - 3));
+
+  if (isComponent(component)) {
+    return component;
+  }
+
+  throw new TypeError('object is not a Component');
+};
