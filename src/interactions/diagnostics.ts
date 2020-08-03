e5a5b12a083a6f9358e962b4692c5fb77f851c75
Martijn Jansen <martijn.jansen@bettyblocks.com>
2020-08-03 15:15:40 +0200

    feat: use a typescript configuration for transpiling

--- src/interactions/diagnostics.ts
+++ src/interactions/diagnostics.ts
@@ -1,10 +1,16 @@
+import { readFileSync } from 'fs';
+import * as path from 'path';
+
 import {
   createProgram,
   Diagnostic,
   flattenDiagnosticMessageText,
   getPreEmitDiagnostics,
   getDefaultCompilerOptions,
-  CompilerOptions,
+  parseJsonConfigFileContent,
+  parseConfigFileTextToJson,
+  sys,
+  ParsedCommandLine,
 } from 'typescript';
 
 function reportDiagnostics(diagnostics: Diagnostic[]): void {
@@ -24,20 +30,44 @@ function reportDiagnostics(diagnostics: Diagnostic[]): void {
   });
 }
 
-export default function(
-  filePath: string,
-  compilerOptions?: CompilerOptions,
-): void {
-  const options = compilerOptions || getDefaultCompilerOptions();
+function getCompilerOptions(): ParsedCommandLine {
+  const fileName = `${__dirname}/../../compilerOptions.json`;
+  const configFileString = readFileSync(fileName).toString();
 
-  options.strict = true;
+  const result = parseConfigFileTextToJson(fileName, configFileString);
 
-  const program = createProgram([filePath], options);
+  if (!result.config) {
+    reportDiagnostics([...(result.error ? [result.error] : [])]);
+    process.exit(1);
+  }
+
+  const parsedConfig = parseJsonConfigFileContent(
+    result.config,
+    sys,
+    path.dirname(fileName),
+  );
+
+  if (parsedConfig.errors.length > 0) {
+    reportDiagnostics(parsedConfig.errors);
+    process.exit(1);
+  }
+
+  return parsedConfig;
+}
+
+export default function(filePath: string): void {
+  const config = getCompilerOptions();
+
+  const program = createProgram(
+    [filePath],
+    config.options || getDefaultCompilerOptions(),
+  );
   const emitResult = program.emit();
 
-  const diagnostics = getPreEmitDiagnostics(program).concat(
-    emitResult.diagnostics,
-  );
+  const diagnostics = [
+    ...getPreEmitDiagnostics(program),
+    ...emitResult.diagnostics,
+  ];
 
   if (diagnostics.length > 0) {
     reportDiagnostics(diagnostics);
