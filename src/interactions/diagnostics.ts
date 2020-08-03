4a696295d6f6ef9f374f628c9b4f1cce2fe8e705
Martijn Jansen <martijn.jansen@bettyblocks.com>
2020-08-03 11:53:50 +0200

    feat: run typescript compiler for compiler feedback

---
+++ src/interactions/diagnostics.ts
@@ -0,0 +1,48 @@
+import { readFileSync } from 'fs';
+
+import {
+  createProgram,
+  Diagnostic,
+  flattenDiagnosticMessageText,
+  getPreEmitDiagnostics,
+  getDefaultCompilerOptions,
+  CompilerOptions,
+} from 'typescript';
+
+function reportDiagnostics(diagnostics: Diagnostic[]): void {
+  diagnostics.forEach(diagnostic => {
+    let message = 'Error';
+    if (diagnostic.file) {
+      const { line, character } = diagnostic.file.getLineAndCharacterOfPosition(
+        diagnostic.start || 0,
+      );
+      message += ` ${diagnostic.file.fileName} (${line + 1},${character + 1})`;
+    }
+    message += `: ${flattenDiagnosticMessageText(
+      diagnostic.messageText,
+      '\n',
+    )}`;
+    console.error(`\u001b[31m${message}\u001b[0m`);
+  });
+}
+
+export default function(
+  filePath: string,
+  compilerOptions?: CompilerOptions,
+): void {
+  const options = compilerOptions || getDefaultCompilerOptions();
+
+  options.strict = true;
+
+  const program = createProgram([filePath], options);
+  const emitResult = program.emit();
+
+  const diagnostics = getPreEmitDiagnostics(program).concat(
+    emitResult.diagnostics,
+  );
+
+  if (diagnostics.length > 0) {
+    reportDiagnostics(diagnostics);
+    process.exit(1);
+  }
+}
