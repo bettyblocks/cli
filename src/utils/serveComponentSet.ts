0551bd119107ff6055941747c3eb5897e823e973
Jesper Dolieslager <j.dolieslager@techrebels.io>
2020-08-10 20:46:54 +0200

    feat: support https, proper error handling

--- src/utils/serveComponentSet.ts
+++ src/utils/serveComponentSet.ts
@@ -1,49 +1,60 @@
-import { createServer, Server, IncomingMessage, ServerResponse } from 'http';
+import { IncomingMessage, ServerResponse } from 'http';
+import { createServer, ServerOptions } from 'https';
 import { existsSync } from 'fs';
-import chalk from 'chalk';
 import handler from 'serve-handler';
-
+import * as fs from 'fs';
 import { checkUpdateAvailableCLI } from './checkUpdateAvailable';
+import { ServeOptions } from '../types';
 
-const serveComponentSet = (rootDir: string, port: number): void => {
-  const server: Server = createServer(
-    (response: IncomingMessage, request: ServerResponse): Promise<void> =>
-      handler(response, request, {
-        public: `${rootDir}/dist`,
-        headers: [
-          {
-            source: '**/*.@(json)',
-            headers: [
-              {
-                key: 'Access-Control-Allow-Origin',
-                value: '*',
-              },
-              {
-                key: 'Cache-Control',
-                value: 'no-cache ',
-              },
-            ],
-          },
-        ],
-      }),
-  );
+const serveComponentSet = (options: ServeOptions): Promise<void> => {
+  return new Promise<void>((resolve, reject) => {
+    const serverOptions: ServerOptions = {};
 
-  server.listen(port, (): void => {
-    console.info(
-      chalk.green(`Serving the component set at http://localhost:${port}`),
-    );
+    if (options.ssl) {
+      if (!fs.existsSync(options.sslKey)) {
+        throw new Error(`Private key '${options.sslKey}' does not exists.`);
+      }
+
+      if (!fs.existsSync(options.sslCert)) {
+        throw new Error(`Certificate '${options.sslCert}' does not exists.`);
+      }
+
+      serverOptions.key = fs.readFileSync(options.sslKey);
+      serverOptions.cert = fs.readFileSync(options.sslCert);
+    }
+
+    createServer(
+      serverOptions,
+      (response: IncomingMessage, request: ServerResponse): Promise<void> =>
+        handler(response, request, {
+          public: `${options.rootDir}/dist`,
+          headers: [
+            {
+              source: '**/*.@(json)',
+              headers: [
+                {
+                  key: 'Access-Control-Allow-Origin',
+                  value: response.headers.origin || '*',
+                },
+                {
+                  key: 'Cache-Control',
+                  value: 'no-cache ',
+                },
+              ],
+            },
+          ],
+        }),
+    )
+      .on('error', error => reject(error.message))
+      .listen(options.port, options.host, () => resolve());
   });
 };
 
-export default async (rootDir: string, port: number): Promise<void> => {
+export default async (options: ServeOptions): Promise<void> => {
   await checkUpdateAvailableCLI();
-  if (existsSync(`${rootDir}/dist`)) {
-    serveComponentSet(rootDir, port);
-  } else {
-    console.error(
-      chalk.red(
-        '\nAn error has occurred, please check if something went wrong during the build step.\n',
-      ),
-    );
+  if (!existsSync(`${options.rootDir}/dist`)) {
+    throw new Error(`Directory '${options.rootDir}/dist' does not exists.`);
   }
+
+  return serveComponentSet(options);
 };
