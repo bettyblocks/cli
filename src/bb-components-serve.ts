d5d8bdad1019ce7a0cf7409befe464f27ed6a97c
Jesper Dolieslager <j.dolieslager@techrebels.io>
2020-08-10 19:51:15 +0200

    feat: add support for https and different hostname

--- src/bb-components-serve.ts
+++ src/bb-components-serve.ts
@@ -1,9 +1,10 @@
 /* npm dependencies */
 
-import program, { CommanderStatic } from 'commander';
+import program from 'commander';
 
 import { parseDir, parsePort } from './utils/arguments';
 import serveComponentSet from './utils/serveComponentSet';
+import { ServeOptions } from './types';
 
 /* internal dependencies */
 
@@ -12,12 +13,21 @@ import serveComponentSet from './utils/serveComponentSet';
 program
   .usage('[path]')
   .name('bb components serve')
-  .option('-p, --port [port]', 'Serve on a custom port. Defaults to 5001.')
+  .option('-p, --port [port]', 'Port to listen on.', value => parsePort(value, 5001), 5001)
+  .option('--host [host]', 'Host to listen on.', 'localhost')
+  .option('--ssl', 'Serve using HTTPS.', false)
+  .option('--ssl-key [ssl-key]', 'SSL certificate to use for serving HTTPS.')
+  .option('--ssl-cert [ssl-cert]', 'SSL key to use for serving HTTPS.')
   .parse(process.argv);
 
-const { args, port: portRaw }: CommanderStatic = program;
-const rootDir: string = parseDir(args);
-const port: number = parsePort(portRaw, 5001);
+const options: ServeOptions = {
+  rootDir: parseDir(program.args),
+  port: program.port,
+  host: program.host,
+  ssl: program.ssl,
+  sslKey: program.sslKey,
+  sslCert: program.sslCert,
+};
 
 /* execute command */
-serveComponentSet(rootDir, port);
+serveComponentSet(options);
