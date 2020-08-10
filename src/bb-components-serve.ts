e4d69789e110cb8429a3863732e8617f7fbbf56d
Jesper Dolieslager <j.dolieslager@techrebels.io>
2020-08-10 21:52:02 +0200

    fix: eslint warnings

--- src/bb-components-serve.ts
+++ src/bb-components-serve.ts
@@ -1,11 +1,10 @@
 /* npm dependencies */
 
+import chalk from 'chalk';
 import program from 'commander';
-
 import { parseDir, parsePort } from './utils/arguments';
 import serveComponentSet from './utils/serveComponentSet';
 import { ServeOptions } from './types';
-import chalk from 'chalk';
 
 /* internal dependencies */
 
@@ -14,7 +13,12 @@ import chalk from 'chalk';
 program
   .usage('[path]')
   .name('bb components serve')
-  .option('-p, --port [port]', 'Port to listen on.', value => parsePort(value, 5001), 5001)
+  .option(
+    '-p, --port [port]',
+    'Port to listen on.',
+    value => parsePort(value, 5001),
+    5001,
+  )
   .option('--host [host]', 'Host to listen on.', 'localhost')
   .option('--ssl', 'Serve using HTTPS.', false)
   .option('--ssl-key [ssl-key]', 'SSL certificate to use for serving HTTPS.')
@@ -43,4 +47,3 @@ serveComponentSet(options).then(
     process.exit(1);
   },
 );
-
