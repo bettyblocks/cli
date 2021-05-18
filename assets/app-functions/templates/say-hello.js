892837a99ac088d9aae7243a8c4a9ac0c49fa8e7
Daniel Willemse <daniel.c.m.willemse@gmail.com>
2021-05-18 16:18:53 +0200

    feat: update sayHello template to reflect latest ActionsJS (#220)
    Authored-by: Daniel Willemse <daniel.willemse@bettyblocks.com>

--- assets/app-functions/templates/say-hello.js
+++ assets/app-functions/templates/say-hello.js
@@ -1,11 +1,13 @@
 import join from 'lodash/join';
 
-export const sayHello = async (ctx, { name }) => {
+const sayHello = async ({ name }) => {
   if (name === 'oops') {
     throw new Error('Ooops. Something went wrong.');
   } else {
     return {
-      greet: join(['Hello', name], ',')
+      greet: join(['Hello', name], ', ')
     };
   }
 }
+
+export default sayHello;
