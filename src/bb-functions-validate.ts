ca4391689758c528ec9f38c6110e10178e343d45
Daniel Willemse <daniel.willemse@bettyblocks.com>
2021-07-22 17:12:41 +0200

    feat: move printing logic back to bb-functions-validate

--- src/bb-functions-validate.ts
+++ src/bb-functions-validate.ts
@@ -1,12 +1,18 @@
 /* npm dependencies */
 
+import chalk from 'chalk';
 import fs from 'fs-extra';
 import path from 'path';
 import program from 'commander';
+import { Validator } from 'jsonschema';
 
 /* internal dependencies */
 
-import { functionValidator, validateFunction } from './utils/validateFunction';
+import {
+  fetchFunction,
+  functionValidator,
+  validateFunction,
+} from './utils/validateFunction';
 
 /* process arguments */
 program
@@ -15,22 +21,40 @@ program
   .parse(process.argv);
 
 const {
-  args: [functionName],
+  args: [inputFunctionName],
 } = program;
 /* execute command */
 
 const workingDir = process.cwd();
 const baseFunctionsPath = path.join(workingDir, 'functions');
 
+const validateFunctionByName = async (
+  functionName: string,
+  validator: Validator,
+): Promise<void> => {
+  const functionPath = path.join(baseFunctionsPath, functionName);
+  const json = await fetchFunction(functionPath);
+  validateFunction(json, validator).then(
+    ({ status, functionName: name, errors }) => {
+      if (status === 'ok') {
+        const mark = chalk.green(`√`);
+        console.log(`${mark} Validated: ${name}`);
+      } else {
+        const msg = chalk.red(`${errors}`);
+        const mark = chalk.red(`x`);
+        console.log(`${mark} Validated: ${name}\n\t${msg}`);
+      }
+    },
+  );
+};
+
 (async (): Promise<void> => {
   const validator = await functionValidator();
-  if (functionName) {
-    const functionPath = path.join(baseFunctionsPath, functionName);
-    validateFunction(functionPath, validator);
+  if (inputFunctionName) {
+    validateFunctionByName(inputFunctionName, validator);
   } else {
     fs.readdirSync(baseFunctionsPath).forEach(functionDir => {
-      const functionPath = path.join(baseFunctionsPath, functionDir);
-      validateFunction(functionPath, validator);
+      validateFunctionByName(functionDir, validator);
     });
   }
 })();
--- src/utils/validateFunction.ts
+++ src/utils/validateFunction.ts
@@ -1,10 +1,9 @@
 import fs from 'fs-extra';
-import chalk from 'chalk';
 import path from 'path';
 import fetch from 'node-fetch';
-import { Validator, ValidatorResult } from 'jsonschema';
+import { Validator, ValidatorResult, ValidationError } from 'jsonschema';
 
-export type FunctionDefinition = {
+type FunctionDefinition = {
   id?: string;
   name?: string;
 };
@@ -13,7 +12,7 @@ export type Schema = {
   $id: string;
 };
 
-export type Config = {
+type Config = {
   schemaUrl: string;
   functionSchemaPath: string;
 };
@@ -23,19 +22,17 @@ const config = fs.readJSONSync(
 ) as Config;
 const { schemaUrl: baseSchemaUrl, functionSchemaPath } = config;
 
-const functionExists = async (
+const fetchFunction = async (
   functionPath: string,
 ): Promise<FunctionDefinition> => {
-  let json: FunctionDefinition;
   const filePath = path.join(functionPath, 'function.json');
   try {
     const exists = await fs.pathExists(filePath);
     if (!exists) throw new Error(`file not found ${filePath}`);
-    json = await fs.readJSON(filePath);
+    return (await fs.readJSON(filePath)) as FunctionDefinition;
   } catch (err) {
     throw new Error(`could not load json from ${functionPath}: ${err}`);
   }
-  return json;
 };
 
 const fetchRemoteSchema = async (schemaUrl: string): Promise<Schema> => {
@@ -71,46 +68,36 @@ const functionValidator = async (): Promise<Validator> => {
 
 const validateFunctionDefinition = async (
   validator: Validator,
-  functionSchema: Schema,
   functionDefinition: FunctionDefinition,
 ): Promise<ValidatorResult> => {
+  const functionSchemaId = Object.keys(validator.schemas).find(k => {
+    return k.match(/function\.json$/);
+  });
+
+  if (!functionSchemaId) {
+    throw new Error(`Cannot find Function schema Id, ${functionSchemaId}`);
+  }
+
+  const functionSchema = validator.schemas[functionSchemaId] as Schema;
   return validator.validate(functionDefinition, functionSchema);
 };
 
 const validateFunction = async (
-  functionPath: string,
+  functionJson: FunctionDefinition,
   validator: Validator,
-): Promise<void> => {
-  const functionSchemaId = Object.keys(validator.schemas).find(k => {
-    return k.match(/function\.json$/);
-  });
-  if (!functionSchemaId) {
-    throw new Error(`Cannot find Function schema Id, ${functionSchemaId}`);
-  }
-  const functionSchema = validator.schemas[functionSchemaId] as Schema;
-
-  functionExists(functionPath)
-    .then(json => {
-      const functionName = json.name || functionPath;
-
-      validateFunctionDefinition(validator, functionSchema, json).then(
-        ({ errors }) => {
-          if (errors.length) {
-            const msg = chalk.red(`${errors}`);
-            const status = chalk.red(`x`);
-            console.log(`${status} Validated: ${functionName}\n\t${msg}`);
-          } else {
-            const status = chalk.green(`√`);
-            console.log(`${status} Validated: ${functionName}`);
-          }
-        },
-      );
-    })
-    .catch(({ message }) => {
-      const status = chalk.red(`x`);
-      const msg = chalk.red(message);
-      console.log(`${status} Validated: ${functionPath}\n\t${msg}`);
-    });
+): Promise<{
+  status: string;
+  functionName?: string;
+  errors?: ValidationError[];
+}> => {
+  return validateFunctionDefinition(validator, functionJson).then(
+    ({ errors }) => {
+      if (errors.length) {
+        return { status: 'error', functionName: functionJson.name, errors };
+      }
+      return { status: 'ok', functionName: functionJson.name };
+    },
+  );
 };
 
-export { functionValidator, validateFunction };
+export { fetchFunction, functionValidator, validateFunction };
