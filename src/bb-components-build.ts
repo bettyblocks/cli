0dda131cae17be76f09bf6044a8f49ce09b22268
Martijn Jansen <martijn.jansen@bettyblocks.com>
2021-09-13 10:49:16 +0200

    feat: remove transformers from dev version

--- src/bb-components-build.ts
+++ src/bb-components-build.ts
@@ -76,7 +76,7 @@ const readComponents: () => Promise<Component[]> = async (): Promise<
 
           transpiledFunction.transpiledDevJsx = doTranspile(
             transpiledFunction.jsx,
-            [stripHandlers, ...transformers],
+            [stripHandlers],
           );
 
           transpiledFunction.transpiledStyles = doTranspile(
