2236ecc8943a95c929e210388eb267eba7e2196e
Thomas van Ophem <thomas.vanophem@gmail.com>
2021-10-25 10:03:14 +0200

    Task/generate index for app functions actions 253 (#256)
    * feat: generate index.js and add to zip file
    
    * fix: fix linter issues in tests

--- __tests__/functions/functionDefinition.test.ts
+++ __tests__/functions/functionDefinition.test.ts
@@ -8,7 +8,7 @@ import {
   functionDefinitionPath,
   isFunctionDefinition,
   newFunctionDefinition,
-  stringifyDefinitions
+  stringifyDefinitions,
 } from '../../src/functions/functionDefinitions';
 
 type Context = ExecutionContext<unknown>;
@@ -17,14 +17,17 @@ const supportDir = path.join(process.cwd(), '__tests__/support/');
 const functionsPath = path.join(supportDir, 'functions');
 
 test('functionDefinitionPath', async (t: Context): Promise<void> => {
-  t.is(functionDefinitionPath('/functions/loop'), '/functions/loop/function.json');
+  t.is(
+    functionDefinitionPath('/functions/loop'),
+    '/functions/loop/function.json',
+  );
 });
 
 test('isFunctionDefinition', async (t: Context): Promise<void> => {
   const correctPath = path.join(functionsPath, 'say-hello');
   t.true(isFunctionDefinition(correctPath));
 
-  const incorrectPath = path.join(functionsPath, 'say-goodbye')
+  const incorrectPath = path.join(functionsPath, 'say-goodbye');
   t.false(isFunctionDefinition(incorrectPath));
 });
 
@@ -32,40 +35,44 @@ test('functionDefinition', async (t: Context): Promise<void> => {
   const functionPath = path.join(functionsPath, 'say-hello');
   t.like(functionDefinition(functionPath), {
     schema: {
-      name: 'sayHello'
-    }
+      name: 'sayHello',
+    },
   });
 });
 
 test('creating a new functionDefinition', async (t: Context): Promise<void> => {
-  const tmpFunctionsDir = 'tmpFunctions' + Math.random().toString()
+  const tmpFunctionsDir = `tmpFunctions${Math.random().toString()}`;
   const tmpFunctionsPath = path.join(supportDir, tmpFunctionsDir);
   newFunctionDefinition(tmpFunctionsPath, 'ciao-mondo');
 
   const functionPath = path.join(tmpFunctionsPath, 'ciao-mondo');
-  const {schema} = functionDefinition(functionPath);
+  const { schema } = functionDefinition(functionPath);
 
   fs.removeSync(tmpFunctionsPath);
   t.like(schema, {
-    name: 'ciaoMondo'
+    name: 'ciaoMondo',
   });
 });
 
-test('functionDefinitions for a directory with functions', async (t: Context): Promise<void> => {
-  const [{schema}] = functionDefinitions(functionsPath)
+test('functionDefinitions for a directory with functions', async (t: Context): Promise<
+  void
+> => {
+  const [{ schema }] = functionDefinitions(functionsPath);
   t.like(schema, {
-      name: 'sayHello'
-    }
-  );
+    name: 'sayHello',
+  });
 });
 
-test('functionDefinitions for a directory without functions', async (t: Context): Promise<void> => {
+test('functionDefinitions for a directory without functions', async (t: Context): Promise<
+  void
+> => {
   const wrongFunctionsPath = supportDir;
   t.deepEqual(functionDefinitions(wrongFunctionsPath), []);
 });
 
 test('stringifying function definitions', async (t: Context): Promise<void> => {
-  const expected = '{"sayHello":{"description":"Say Hello to the world","name":"sayHello","label":"Say Hello","category":"Misc","icon":"CreateIcon","options":"[{\\"meta\\":{\\"type\\":\\"String\\",\\"io\\":\\"in\\"},\\"name\\":\\"name\\",\\"label\\":\\"Name\\",\\"info\\":\\"The name that\'s going to be used to say hello to the world!\\",\\"advanced\\":false,\\"configuration\\":{\\"placeholder\\":\\"Betty Blocks\\"}}]","yields":"none"}}'
+  const expected =
+    '{"sayHello":{"description":"Say Hello to the world","name":"sayHello","label":"Say Hello","category":"Misc","icon":"CreateIcon","options":"[{\\"meta\\":{\\"type\\":\\"String\\",\\"io\\":\\"in\\"},\\"name\\":\\"name\\",\\"label\\":\\"Name\\",\\"info\\":\\"The name that\'s going to be used to say hello to the world!\\",\\"advanced\\":false,\\"configuration\\":{\\"placeholder\\":\\"Betty Blocks\\"}}]","yields":"none"}}';
   const definitions = functionDefinitions(functionsPath);
   t.is(stringifyDefinitions(definitions), expected);
 });
--- __tests__/functions/validateFunction.test.ts
+++ __tests__/functions/validateFunction.test.ts
@@ -1,38 +1,40 @@
 import test, { ExecutionContext } from 'ava';
 import path from 'path';
+import { Validator } from 'jsonschema';
 import { functionDefinition } from '../../src/functions/functionDefinitions';
 import { validateFunction } from '../../src/functions/validations';
-import { Validator } from 'jsonschema';
 
 type Context = ExecutionContext<unknown>;
 
 const schema = {
-  "$id": "/schema/actions/function.json",
-  "title": "Function",
-  "properties": {
-    "name": {
-      "type": "string"
+  $id: '/schema/actions/function.json',
+  title: 'Function',
+  properties: {
+    name: {
+      type: 'string',
     },
-    "icon": {
-      "enum": ["CreateIcon", "DeleteIcon"]
-    }
+    icon: {
+      enum: ['CreateIcon', 'DeleteIcon'],
+    },
   },
-  "required": ["name"]
-}
+  required: ['name'],
+};
 
 const validator = new Validator();
 validator.addSchema(schema, schema.$id);
 
-test('load in entire schema for validator', async (t: Context): Promise<void> => {
+test('load in entire schema for validator', async (t: Context): Promise<
+  void
+> => {
   const definition = {
     path: '/path/to/schema/actions/function.json',
     schema: {
-      "name": "create",
-      "icon": "CreateIcon"
-    }
-  }
+      name: 'create',
+      icon: 'CreateIcon',
+    },
+  };
 
-  const {status, errors} = await validateFunction(definition, validator);
+  const { status, errors } = await validateFunction(definition, validator);
 
   t.is(status, 'ok');
   t.is(errors.length, 0);
@@ -42,35 +44,46 @@ test('validate templates', async (t: Context): Promise<void> => {
   const functionPath = path.join(
     process.cwd(),
     'assets/app-functions/templates',
-    'functions/say-hello'
+    'functions/say-hello',
   );
 
   const functionJson = functionDefinition(functionPath);
-  const {status} = await validateFunction(functionJson, validator);
+  const { status } = await validateFunction(functionJson, validator);
 
   t.is(status, 'ok');
 });
 
 test('invalidate empty schemas', async (t: Context): Promise<void> => {
-  const {status, errors: [{message}]} = await validateFunction({
-    path: '/path/to/schema/actions/function.json',
-    schema: {}
-  }, validator);
+  const {
+    status,
+    errors: [{ message }],
+  } = await validateFunction(
+    {
+      path: '/path/to/schema/actions/function.json',
+      schema: {},
+    },
+    validator,
+  );
 
   t.is(status, 'error');
   t.is(message, 'requires property "name"');
 });
 
-test('invalidate schemas that do not have valid values for properties', async (t: Context): Promise<void> => {
+test('invalidate schemas that do not have valid values for properties', async (t: Context): Promise<
+  void
+> => {
   const definition = {
     path: '/path/to/schema/actions/function.json',
     schema: {
-      "name": "create",
-      "icon": "RandomIcon"
-    }
-  }
+      name: 'create',
+      icon: 'RandomIcon',
+    },
+  };
 
-  const {status, errors: [{message}]} = await validateFunction(definition, validator);
+  const {
+    status,
+    errors: [{ message }],
+  } = await validateFunction(definition, validator);
 
   t.is(status, 'error');
   t.is(message, 'is not one of enum values: CreateIcon,DeleteIcon');
