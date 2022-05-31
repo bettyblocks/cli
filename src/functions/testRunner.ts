6bdf06e5754b493283cc36ab8721fa61ada6c24f
Paul Engel <pm_engel@icloud.com>
2022-05-31 23:59:46 +0200

    feat: provide `test` and `assert` for isolated function tests

--- src/functions/testRunner.ts
+++ src/functions/testRunner.ts
@@ -34,6 +34,7 @@ const build = async (
       name: 'app',
       dependencies: { app: 'file:./app' },
       devDependencies: {
+        lodash: '^4.17.15',
         webpack: '^5.10.0',
         'webpack-cli': '^4.2.0',
       },
@@ -62,7 +63,8 @@ const build = async (
 
   fs.writeFileSync(
     path.join(tmpDir, 'app.js'),
-    `import * as $app from 'app';
+    `import { default as $app } from 'app';
+import _ from 'lodash';
 
 ${fs.readFileSync(path.join(workingDir, testFile), 'utf-8')}
 `,
@@ -110,6 +112,10 @@ const run = (testFile: string, workingDir: string): Promise<string> => {
 
   const script = isolate.compileScriptSync(
     `function run(resolve, reject) {
+  
+  const tests = [];
+  let failures = 0;
+  
   const console = (() => {
     const log = (level) =>
       (...args) =>
@@ -127,28 +133,48 @@ const run = (testFile: string, workingDir: string): Promise<string> => {
     };
   })();
 
+  const test = (desc, fn) => {
+    tests.push(new Promise((resolve, reject) => {
+      fn()
+        .then(() => {
+          console.log('\\x1b[32m%s\\x1b[0m', '\\u2714 test ' + JSON.stringify(desc));
+          resolve();
+        })
+        .catch((e) => {
+          console.log('\\x1b[31m%s\\x1b[0m', '\\u2718 test ' + JSON.stringify(desc));
+          console.error(e.message);
+          resolve();
+          failures++;
+        });
+    }));
+  };
+
+  const assert = (left, right) => {
+    if (!_.isEqual(left, right)) {
+      throw new Error(\`\\x1b[31m  Assertion failed\\x1b[0m
+  \\x1b[36mleft:\\x1b[0m   \${JSON.stringify(left)}
+  \\x1b[36mright:\\x1b[0m  \${JSON.stringify(right)}\`);
+    }
+  };
+
   ${fs.existsSync(helpers) ? fs.readFileSync(helpers, 'utf-8') : ''}
   ${fs.readFileSync(bundle, 'utf-8')}
 
-  app
-    .default()
-    .then((result) => {
-      resolve.applyIgnored(null, [
-        new ivm.ExternalCopy(JSON.stringify(result)).copyInto(),
-      ]);
-    })
-    .catch((error) => {
-      reject.applyIgnored(null, [
-        new ivm.ExternalCopy(
-          JSON.stringify({
-            error: {
-              message: error.message || error,
-              code: 'RUNTIME_ERROR',
-            },
-          })
-        ).copyInto(),
-      ]);
-    });;
+  Promise.all(tests).then(() => {
+    let summary;
+    
+    if (failures) {
+      summary = \`\\x1b[31m\${tests.length} tests, \${failures} failure\${
+        failures > 1 ? 's' : ''
+      }\\x1b[0m\`;
+    } else {
+      summary = \`\\x1b[32m\${tests.length} tests, 0 failures\\x1b[0m\`;
+    }
+    
+    resolve.applyIgnored(null, [
+      new ivm.ExternalCopy(summary).copyInto()
+    ]);
+  });
 }
 `,
   );
@@ -175,8 +201,15 @@ const runTest = async (testFile: string, workingDir: string): Promise<void> => {
     return;
   }
 
+  let time: number;
+  const start = () => new Date().getTime();
+  const stop = () => (new Date().getTime() - time) / 1000;
+
   console.log(`${right} Building artifacts ...`);
+
+  time = start();
   const { exitCode, stdout } = await build(testFile, workingDir);
+  const buildTime = stop();
 
   if (exitCode) {
     console.log(`${cross} ${stdout}`);
@@ -185,9 +218,20 @@ const runTest = async (testFile: string, workingDir: string): Promise<void> => {
 
   try {
     console.log(`${check} Build succeeded`);
-    console.log(`${right} Running test ...`);
-    const output = await run(testFile, workingDir);
-    console.log(`${check} ${output}`);
+    console.log(`${right} Running tests ...`);
+
+    time = start();
+    const summary = await run(testFile, workingDir);
+    const testTime = stop();
+
+    console.log(
+      `\nFinished in ${(buildTime + testTime).toFixed(
+        3,
+      )} seconds (build: ${buildTime.toFixed(3)}s, tests: ${testTime.toFixed(
+        3,
+      )})`,
+    );
+    console.log(summary);
   } catch (error) {
     console.log(`${cross} ${String(error)}`);
   }
