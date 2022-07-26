b46b75f368547aa4cd4284c24977310611a19f01
JorisPannekeet <56304920+JorisPannekeet@users.noreply.github.com>
2022-07-26 13:15:35 +0200

    fix: removed unneeded deps and code (#329)

--- assets/component-set/src/utils.ts
+++ assets/component-set/src/utils.ts
@@ -1,17 +1,4 @@
-import { PrefabComponentOption, showIfTrue } from '@betty-blocks/component-sdk';
-
-// TODO: export OptionProducer from the sdk
-type OptionProducer = (key: string) => PrefabComponentOption;
-type Attributes = Partial<PrefabComponentOption>;
-
-export type PartialBy<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>;
-
-export const updateOption = (
-  producer: OptionProducer,
-  attrs: Attributes,
-): OptionProducer => {
-  return (key) => ({ ...producer(key), ...attrs });
-};
+import { showIfTrue } from '@betty-blocks/component-sdk';
 
 export const showOn = (key: string) => ({
   configuration: { condition: showIfTrue(key) },
