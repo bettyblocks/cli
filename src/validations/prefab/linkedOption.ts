c9ad0802c1508133a6d2b6d5fc6fee940f833c97
sventruschel <67267206+sventruschel@users.noreply.github.com>
2022-09-07 11:56:16 +0200

    fix: fix allowed types typing (#343)
    * fix: fix allowed types typing
    
    * fix: added number as allowed input and fixed other types
    
    * fix: make the optionid optional because of wrapper
    
    * fix: expand PrefabComponentOptionCategory

--- src/validations/prefab/linkedOption.ts
+++ src/validations/prefab/linkedOption.ts
@@ -4,12 +4,19 @@ import { COMPARATORS, CONDITION_TYPE, CONFIGURATION_AS } from '../constants';
 const linkedOptionValueSchema = Joi.object({
   ref: Joi.object({
     componentId: Joi.string().required(),
-    optionId: Joi.string().required(),
+    optionId: Joi.string().optional(),
   }),
 });
 
 export const linkedOptionConfigurationSchema = Joi.object({
   as: Joi.string().valid(...CONFIGURATION_AS),
+  dataType: Joi.string(),
+  allowedInput: Joi.array().items(
+    Joi.object({
+      name: Joi.string(),
+      value: Joi.alternatives().try(Joi.boolean(), Joi.string(), Joi.number()),
+    }),
+  ),
   condition: Joi.object({
     type: Joi.string().valid(...CONDITION_TYPE),
     option: Joi.string(),
