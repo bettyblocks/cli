5e44ea96f86ef6e3abfd70a16485d8bccc1993c0
Brian van den Berg <97033729+BettyBrian@users.noreply.github.com>
2023-05-22 09:06:41 +0200

    feat: create publish bundle function (#421)
    * feat: create publish bundle function

---
+++ src/functions/bb-components-functions.ts
@@ -0,0 +1,50 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import uploadBlob, {
+  BlockBlobUploadResponseExtended,
+} from 'src/utils/uploadBlob';
+import chalk from 'chalk';
+
+export const upload = async (
+  objects: unknown,
+  fileName: string,
+  bucketName: string,
+): Promise<BlockBlobUploadResponseExtended> => {
+  try {
+    return await uploadBlob(bucketName, fileName, JSON.stringify(objects));
+  } catch (error) {
+    const defaultMessage =
+      'There was an error trying to publish your component set';
+    const { body, message } = error;
+
+    if (!body) {
+      throw new Error(chalk.red([defaultMessage, message].join('\n')));
+    }
+
+    const { code, message: bodyMessage } = body;
+
+    const extraMessage =
+      code === 'AuthenticationFailed'
+        ? 'Make sure your azure blob account and key are correct'
+        : bodyMessage;
+
+    throw new Error(chalk.red([defaultMessage, extraMessage].join('\n')));
+  }
+};
+
+export const publish = async (
+  fileName: string,
+  bucketName: string,
+  read: (fileName: string) => Promise<void>,
+): Promise<BlockBlobUploadResponseExtended> => {
+  console.log(`Publishing ${fileName}.`);
+
+  const objects = await read(fileName);
+
+  return upload(objects, fileName, bucketName);
+};
+
+export const validateBucketName = (name: string) => {
+  if (!name || typeof name !== 'string' || !name.length) {
+    throw new Error(chalk.red('\n-b or --bucket [name] is required\n'));
+  }
+};
