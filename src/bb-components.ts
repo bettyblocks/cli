34c6f9781a0ee3d2b0a52763f87a485d3e3a9899
Sander <sander.vanriessen@bettyblocks.com>
2023-06-01 13:43:13 +0200

    fix: revert publish command

--- src/bb-components.ts
+++ src/bb-components.ts
@@ -14,7 +14,7 @@ const availableCommands: CommandComponents[] = [
   'publish',
   'help',
   'generate',
-  'publishbundle',
+  'publish-bundle',
 ];
 
 /* process arguments */
@@ -31,7 +31,7 @@ program
     'serve [path]',
     'serve the component set at a specific path, defaults to CWD',
   )
-  .command('publishbundle [options] [path]', 'publish a bundle')
+  .command('publish-bundle [options] [path]', 'publish a bundle')
   .command(
     'publish [options] [path]',
     'publish the component set from a specific path, defaults to CWD',
