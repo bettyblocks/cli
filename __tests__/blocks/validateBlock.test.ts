931fd11695a4f6f1230c576f37571bf790fa5bb7
Emiel Wit <emiel.wit@bettyblocks.com>
2023-06-21 13:23:06 +0200

    test: add tests

--- __tests__/blocks/validateBlock.test.ts
+++ __tests__/blocks/validateBlock.test.ts
@@ -1,9 +1,9 @@
-import fs from 'fs-extra';
 import test, { ExecutionContext } from 'ava';
 import {
   validateBlockDependencies,
   getErrorMessage,
   validateBlockConfig,
+  validateBlock,
 } from '../../src/validations/function-block-validations';
 import { Block } from '../../src/blocks/blockDefinitions';
 
@@ -31,6 +31,8 @@ test('It returns an error for invalid functions', (t: Context): void => {
     validFunctions: false,
     validBlockDependencies: true,
     invalidDependencies: [],
+    blockName: 'test-block',
+    validBlockName: true,
   });
 
   t.is(errorMessage, 'One or more functions are not valid');
@@ -41,6 +43,8 @@ test('It returns an error for invalid block dependencies', (t: Context): void =>
     validFunctions: true,
     validBlockDependencies: false,
     invalidDependencies: ['moment'],
+    blockName: 'test-block',
+    validBlockName: true,
   });
 
   t.is(errorMessage, 'The following dependencies are not valid: moment');
@@ -51,6 +55,8 @@ test('It returns an error for invalid block dependencies without package specifi
     validFunctions: true,
     validBlockDependencies: false,
     invalidDependencies: [],
+    blockName: 'test-block',
+    validBlockName: true,
   });
 
   t.is(errorMessage, 'One of the block dependencies is not valid');
@@ -72,4 +78,37 @@ test('It validates if the there is no function defined', (t: Context): void => {
     includes: [],
   };
   t.is(validateBlockConfig(block), false);
+});
+
+test('It validates the block name on kebab case and is not valid', async (t: Context): Promise<void> => {
+  const block: Block = {
+    functions: ['sayHello 1.0'],
+    dependencies: ['lodash'],
+    includes: [],
+  };
+
+  const { valid, errorMessage } = await validateBlock({
+    block,
+    blockFunctions: [],
+    blockName: 'test Block',
+  });
+
+  t.is(valid, false);
+  t.is(errorMessage, 'test Block is not valid as it should be kebab case');
+});
+
+test('It validates the block name on kebab case and is valid', async (t: Context): Promise<void> => {
+  const block: Block = {
+    functions: ['sayHello 1.0'],
+    dependencies: ['lodash'],
+    includes: [],
+  };
+
+  const { valid } = await validateBlock({
+    block,
+    blockFunctions: [],
+    blockName: 'test-block',
+  });
+
+  t.is(valid, true);
 });
